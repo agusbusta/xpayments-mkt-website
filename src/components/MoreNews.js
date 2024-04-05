95755923cad4823c7a2a9f21015dff9abe783c12
agusbustadev <agustin.bustamante@chewedpixelstudios.com>
2024-04-05 17:01:26 -0300

    BTNS ADDED

--- src/components/MoreNews.js
+++ src/components/MoreNews.js
@@ -2,6 +2,13 @@ import React from "react";
 import ethereum from "../assets/ethereum.png";
 import bitcoin from "../assets/bitcoin.png";
 import layer0 from "../assets/layer0.png";
+import layer1 from "../assets/layer1.png";
+import layer2 from "../assets/layer2.png";
+import lsd from "../assets/lsd.png";
+import xpayments from "../assets/xpayments.png";
+import oracles from "../assets/oracles.png";
+import ai from "../assets/ai.png";
+import defi from "../assets/defi.png";
 
 function MoreNews() {
   return (
@@ -31,10 +38,70 @@ function MoreNews() {
             </li>
           </div>
           <div className="website-entire">
-            <li className="website-li layer1div">
+            <li className="website-li layer0div">
               <a href="https://layer0-mkt-website.vercel.app">
-                <img src={layer0} className="layer1logo" alt="Layer 1 (LMC)" />
-                <p className="textCardMoreWebsites">Layer 0</p>
+                <img src={layer0} className="layer0logo" alt="Layer 1 (LMC)" />
+                <p className="textCardMoreWebsites l0">Layer 0</p>
+              </a>
+            </li>
+          </div>
+          <br>
+          </br>
+          <div className="website-entire">
+            <li className="website-li layer1div">
+              <a href="https://layer1-mkt-webiste.vercel.app">
+                <img src={layer1} className="layer1logo" alt="Layer 1 (LMC)" />
+                <p className="textCardMoreWebsites">Layer 1</p>
+              </a>
+            </li>
+          </div>
+          <br></br>
+          
+          <div className="website-group">
+            <li className="website-li">
+              <a href="https://layer2-mkt-webiste.vercel.app">
+                <img
+                  src={layer2}
+                  className="otherwebsiteimgp2"
+                  alt="Ethereum"
+                />
+              </a>
+            </li>
+            <li className="website-li">
+              <a href="https://lsd-mkt-webiste.vercel.app">
+                <img src={lsd} className="otherwebsiteimgp2" alt="Layer 0" />
+              </a>
+            </li>
+          </div>
+          <div className="website-group">
+            <li className="website-li">
+              <a href="https://eth-mkt-webiste.vercel.app">
+                <img
+                  src={xpayments}
+                  className="otherwebsiteimgp2"
+                  alt="Ethereum"
+                />
+              </a>
+            </li>
+            <li className="website-li">
+              <a href="https://oracle-met-website.vercel.app/">
+                <img src={oracles} className="otherwebsiteimgp2" alt="Layer 0" />
+              </a>
+            </li>
+          </div>
+          <div className="website-group">
+            <li className="website-li">
+              <a href="https://defi-mkt-website.vercel.app/">
+                <img
+                  src={defi}
+                  className="otherwebsiteimgp2"
+                  alt="Ethereum"
+                />
+              </a>
+            </li>
+            <li className="website-li">
+              <a href="https://ai-mkt-website.vercel.app">
+                <img src={ai} className="otherwebsiteimgp2" alt="Layer 0" />
               </a>
             </li>
           </div>
